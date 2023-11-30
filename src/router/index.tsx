df141cf23606cbbb65b154d9128a7b670edefa7b
Luis Garcia <74296603+luisgarcia96@users.noreply.github.com>
2023-11-30 23:11:39 +0100

    Add simple NotFound page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "../pages/LandingPage/LandingPage";
+import NotFound from "../pages/NotFound/NotFound";
 
 const AppRouter = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<LandingPage />} />
-			{/* <Route path="/about" element={<About />} /> */}
-			{/* <Route path="*" element={<NotFound />} /> */}
+			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
 };
