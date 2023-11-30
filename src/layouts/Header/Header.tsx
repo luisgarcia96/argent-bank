2e947d9470246bff314945fb02975179d5d470a6
Luis Garcia <74296603+luisgarcia96@users.noreply.github.com>
2023-11-30 21:53:58 +0100

    Add assets + icon

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import styles from './Header.module.scss';
+import styles from "./Header.module.scss";
 
 const Header: React.FC = () => {
-  return (
-    <header className={styles.header}>
-      <h1>My Header</h1>
-    </header>
-  );
+	return (
+		<header className={styles.header}>
+			<h1>My Header</h1>
+			<i className="fa-solid fa-circle-user"></i>
+		</header>
+	);
 };
 
 export default Header;
