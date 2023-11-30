1a151a33860fffa262f6ee9c209e77ef1618c2cb
Luis Garcia <74296603+luisgarcia96@users.noreply.github.com>
2023-11-30 23:26:22 +0100

    Create Footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import AppRouter from "./router";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./layouts/Header/Header";
 import "./styles/main.scss";
+import Footer from "./layouts/Footer/Footer";
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 				<main>
 					<AppRouter />
 				</main>
+				<Footer />
 			</BrowserRouter>
 		</div>
 	);
