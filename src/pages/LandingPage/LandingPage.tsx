3f60637ec738d1b55ed087ce85d2597a3e3b2520
Luis Garcia <74296603+luisgarcia96@users.noreply.github.com>
2023-12-01 15:48:53 +0100

    Finish Hero component

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,8 +1,10 @@
+import Hero from "../../components/Hero/Hero";
 import styles from "./LandingPage.module.scss";
 
 const LandingPage = () => {
 	return (
 		<div className={styles.container}>
+			<Hero />
 			<h1 className={styles.title}>Welcome to the Landing Page</h1>
 			<p className={styles.description}>
 				This is the description of the landing page.
